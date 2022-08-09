54451098f4b4d0e8fb832f8f649eefacee6c61ba
Cory Lotz <cotz1995@gmail.com>
2022-08-09 18:46:01 -0400

    adds alternate and circleish
    Co-authored-by: gsuemith <garrick.suemith@gmail.com>

--- parrots.py
+++ parrots.py
@@ -70,6 +70,28 @@ def chaos():
             print_parrot(randint(1,9))
         print('')
 
+def alternate():
+        for i in range (0, 18):
+            for j in range(0, 18):
+                print_parrot(1)
+            print('')
+            for j in range(0, 18):
+                print_parrot(5)
+            print('')
+
+def circleish(n):
+    for row in generate_circleish_matrix(n):
+        for num in row:
+            print_parrot(num%9+1)
+        print('')
+
+
+def generate_circleish_matrix(n):
+    return [[distance_from_center(i, j, n//2) for j in range(n)]for i in range(n)]
+
+def distance_from_center(a, b, center):
+    return int(((a-center)**2 + (b-center)**2)**.5)
+
 
 def forward(i):
     for j in range(i, 9+i):
