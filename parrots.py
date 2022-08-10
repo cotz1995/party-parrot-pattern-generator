6e377ad85da148938213926c5d422541bc4933f6
cotz1995 <cotz1995@gmail.com>
2022-08-10 17:19:44 -0400

    adds spiral
    Co-authored-by: gsuemith <garrick.suemith@gmail.com>

--- parrots.py
+++ parrots.py
@@ -1,4 +1,5 @@
 from random import randint
+import math
 
 def to_middle():
     for i in range(9):
@@ -79,16 +80,56 @@ def alternate():
                 print_parrot(5)
             print('')
 
-def circleish(n):
-    for row in generate_circleish_matrix(n):
+def parrots_from_matrix(matrix):
+    for row in matrix:
         for num in row:
             print_parrot(num%9+1)
         print('')
 
+def circleish(n):
+    parrots_from_matrix(generate_circleish_matrix(n))
 
+def spiral(n):
+    parrots_from_matrix(generate_spiral_matrix(n))
+    
 def generate_circleish_matrix(n):
     return [[distance_from_center(i, j, n//2) for j in range(n)]for i in range(n)]
 
+def generate_spiral_matrix(n):
+    return [[distance_from_center(i, j, 0) + offset(i, j, 0) for j in range(-n//2, n//2)]for i in range(-n//2, n//2)]
+
+def generate_coords_matrix(n):
+    return [[(j,i*-1) for j in range(-n//2, n//2)]for i in range(-n//2, n//2)]
+
+def offset(a, b, center):
+    angle = determine_angle(a, b, center)
+    return int(angle * 9 / 2 / math.pi ) + 1
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+def determine_angle(a, b, center):
+    y = (a - center)*-1
+    x = b - center
+    if x == 0:
+        if y == 0:
+            return 0
+        return math.pi / 2 * (-1 if y < 0 else 1)
+    angle = math.atan(y/x)
+    if angle < 0:
+        if y < 0:
+            return angle + math.pi*2
+        else:
+            return angle + math.pi
+    else:
+        if x < 0:
+            return angle + math.pi
+        else:
+            return angle
+    #  - + | + +
+    # ------------
+    #  - - | + -
 def distance_from_center(a, b, center):
     return int(((a-center)**2 + (b-center)**2)**.5)
 
